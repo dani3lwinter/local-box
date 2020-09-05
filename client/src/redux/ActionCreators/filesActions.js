1c01d107a978836104934a19dc31119990b3bd8d
Daniel Winter <daniwin.4@gmail.com>
2020-09-05 19:17:38 +0300

    serverless static demo - all fetch functions replaced with hardcoded json objects.

--- client/src/redux/ActionCreators/filesActions.js
+++ client/src/redux/ActionCreators/filesActions.js
@@ -3,6 +3,10 @@ import { baseUrl } from '../../baseUrl';
 
 // Action Creators:
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export const loadingFiles = loadingState => ({
   type: ActionTypes.LOADING_STATE_FILES,
   payload: loadingState
@@ -16,23 +20,37 @@ export const errorFiles = error => ({
 export const fetchFiles = () => dispatch => {
     dispatch(loadingFiles(true))
 
-  return fetch(baseUrl + 'api/files/', {
-      method:'GET',
-      credentials: "same-origin"
-  })
-  .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        var error = new Error('Error ' + response.status + ': ' + response.statusText);
-        error.response = response;
-        throw error;
-      }
+  return sleep(1000)
+  .then(() => {
+    return [{"encrypted":false,
+      "_id":"101",
+      "originalname":"README.md",
+      "path":"uploads\\README.md",
+      "size":1,
+      "mimetype":"text/plain",
+      "createdAt":"2020-09-05T11:32:49.832Z",
+      "updatedAt":"2020-09-05T11:32:49.832Z",
     },
-    error => {
-          throw error;
-    })
-  .then(response => response.json())
+    {"encrypted":true,
+      "_id":"102",
+      "originalname":"encrypted.txt",
+      "path":"uploads\\encrypted.txt",
+      "size":0,
+      "mimetype":"text/plain",
+      "destroyAt":new Date(Date.now() + (8*60*60*1000)),
+      "createdAt":"2020-09-05T11:32:49.832Z",
+      "updatedAt":"2020-09-05T11:32:49.832Z",
+    },
+    {"encrypted":false,
+      "_id":"103",
+      "originalname":"logo.jpg",
+      "path":"uploads\\logo.jpg",
+      "size":416506,
+      "mimetype": "image/jpeg",
+      "createdAt":"2020-09-05T11:32:49.832Z",
+      "updatedAt":"2020-09-05T11:32:49.832Z",
+    }]
+  })
   .then(json => dispatch(loadFiles(json)))
   .catch(error =>  { 
     error.type = 'connection';
@@ -58,24 +76,8 @@ export const deleteFileFromStore = (fileID) => ({
 
 export const deleteFile = (fileID) => (dispatch) => {
     dispatch(loadingFiles(true))
-    return fetch(baseUrl + 'api/files/download/'+fileID, {
-        method:'DELETE',
-        credentials: "same-origin"
-    })
-    .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
-      error => {
-            throw error;
-      })
-    .then(response => response.json())
-    .then(response => dispatch(deleteFileFromStore(response._id)))
+    return sleep(500)
+    .then(() => dispatch(deleteFileFromStore(fileID)))
     .catch(error =>  { 
       error.type = 'connection';
       error.message = 'Unable to delete file\nError: '+error.message;
@@ -93,24 +95,19 @@ export const postFiles = (uploadForm) => (dispatch) => {
   data.append('password', uploadForm.password);
   data.append('selfDestruct', uploadForm.selfDestruct);
   
-  fetch(baseUrl + "api/files/upload", {
-        method: 'POST',
-        headers: { 'Accept': 'application/json' },
-        body: data
+  sleep(1000)
+  .then(() => {
+    return [{"encrypted":uploadForm.encryptChecked,
+      "_id": "abc"+Math.floor(Math.random() * 10000),
+      "originalname":uploadForm.files[0].name,
+      "path":"uploads\\"+uploadForm.files[0].name,
+      "size":uploadForm.files[0].size,
+      "mimetype":uploadForm.files[0].type,
+      "destroyAt":new Date(Date.now() + (uploadForm.selfDestruct*60*60*1000)),
+      "createdAt":new Date(Date.now()),
+      "updatedAt":new Date(Date.now()),
+    }]
   })
-  .then(response => {
-    if (response.ok) {
-      return response;
-    } else {
-      var error = new Error('Error ' + response.status + ': ' + response.statusText);
-      error.response = response;
-      throw error;
-    }
-  },
-  error => {
-        throw error;
-  })
-  .then(response => response.json())
   .then(response => dispatch(addFile(response)))
   .catch(error =>  { 
       error.type = 'connection';
--- client/src/redux/ActionCreators/notesActions.js
+++ client/src/redux/ActionCreators/notesActions.js
@@ -1,5 +1,11 @@
 import * as ActionTypes from '../actionTypes';
 import { baseUrl } from '../../baseUrl';
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 // Action Creators:
 
 export const editNote = (note) => ({
@@ -20,23 +26,53 @@ export const errorNotes = error => ({
 export const fetchNotes = (id) => dispatch => {
     dispatch(loadingNotes(true))
     var url = id ? baseUrl + 'api/notes/' + id : baseUrl + 'api/notes/';
-    return fetch(url, {
-        method:'GET',
-        credentials: "same-origin"
-    })
-    .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
+    return sleep(1000)
+    .then(() => {
+      return [{
+        "selfDestruct": null,
+        "isMainNote": true,
+        "_id": "5ea0ac00036b3e587cab774b",
+        "content": "",
+        "createdAt": "2020-04-22T20:41:36.258Z",
+        "updatedAt": "2020-09-05T10:58:54.281Z",
       },
-      error => {
-            throw error;
-      })
-    .then(response => response.json())
+      {
+        "selfDestruct": 72,
+        "isMainNote": false,
+        "_id": "5f53b32431b5cb81a4dd1d0c",
+        "title": "README",
+        "content": "Local-Box\nStore files and notes on a local website, and access them within other devices on the same network.\n\nRequirements\n   Node.js\n   mongoDB\n\nUsage\n   1. Start mongoDB instance on the defualt port 27017\n   2. Open command line on 'server' folder and type npm start\n   3. Access the website via http://localhost:3000/",
+        "createdAt": "2020-09-05T15:47:48.939Z",
+        "updatedAt": "2020-09-05T15:47:48.939Z",
+      },
+      {
+        "selfDestruct": 24,
+        "isMainNote": false,
+        "_id": "5f53b33131b5cb81a4dd1d0d",
+        "title": "Note #2",
+        "content": "You can paste here text and open it later on your phone!",
+        "createdAt": "2020-09-05T15:48:01.649Z",
+        "updatedAt": "2020-09-05T15:48:01.649Z",
+      },
+      {
+        "selfDestruct": 24,
+        "isMainNote": false,
+        "_id": "5f53b33131b5cb81a4dd1123",
+        "title": "Note #3",
+        "content": "",
+        "createdAt": "2020-09-05T15:48:01.649Z",
+        "updatedAt": "2020-09-05T15:48:01.649Z",
+      },
+      {
+        "selfDestruct": 24,
+        "isMainNote": false,
+        "_id": "5f53b33131b5cb81a4d4567",
+        "title": "Note #4",
+        "content": "",
+        "createdAt": "2020-09-05T15:48:01.649Z",
+        "updatedAt": "2020-09-05T15:48:01.649Z",
+      }]
+    })
     .then(json => dispatch(loadNotes(json)))
     .catch(error =>  {
       error.type = 'connection';
@@ -63,24 +99,8 @@ export const deleteNoteFromStore = (noteID) => ({
 
 export const deleteNote = (noteID) => (dispatch) => {
     dispatch(loadingNotes(true))
-    return fetch(baseUrl + 'api/notes/'+noteID, {
-        method:'DELETE',
-        credentials: "same-origin"
-    })
-    .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
-      error => {
-            throw error;
-      })
-    .then(response => response.json())
-    .then(response => dispatch(deleteNoteFromStore(noteID)))
+    return sleep(500)
+    .then(() => dispatch(deleteNoteFromStore(noteID)))
     .catch(error =>  { 
       error.type = 'connection';
       error.message = 'Unable to delete note.\nError: '+error.message;
@@ -92,27 +112,15 @@ export const postNote = (note) => (dispatch) => {
     dispatch(loadingNotes(true))
     const resource = note._id ? 'api/notes/'+note._id : 'api/notes';
     
-    return fetch(baseUrl + resource, {
-        method: note._id ? "PUT" : 'POST',
-        body: JSON.stringify(note),
-        headers: {
-          "Content-Type": "application/json"
-        },
-        credentials: "same-origin"
+    return sleep(500)
+    .then(() => {
+      return {
+        ...note,
+        "_id": note._id ? note._id : "abc"+Math.floor(Math.random() * 10000),
+        "createdAt":new Date(Date.now()),
+        "updatedAt":new Date(Date.now()),
+      }
     })
-    .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
-      error => {
-            throw error;
-      })
-    .then(response => response.json())
     .then(response => note._id ? dispatch(editNote(response)) : dispatch(addNote(response)))
     .catch(error =>  {
       error.type = 'connection';
